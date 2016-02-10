fb02fa9409ba8a797cf21cb3f8fd562a6b14f627
Alex <ardent83@yandex.ru>
2016-02-10 12:33:15 +0200

    add usercabinet

---
+++ js/common.js
@@ -0,0 +1,52 @@
+//Fancybox
+  $(document).ready(function() {
+    $(".pop-up").fancybox({
+        closeBtn: false,
+        // wrapCSS:'wrapPopup'     
+    });
+    $(".fb-close").click(function(){
+      $.fancybox.close();
+    });        
+  }); 
+
+//Slider calculator
+  $(function() {
+    $('#amount').change(function(){
+      var val = $(this).val();
+      $('#slider-range-max1').slider('value',val);
+    });    
+    $( "#slider-range-max1" ).slider({
+      range: "max",
+      value: 300,
+      min: 0,
+      max: 700,
+      slide: function( event, ui ) {
+        $( "#amount1" ).val(ui.value );
+      }
+    });
+    $( "#amount1" ).val($( "#slider-range-max1" ).slider( "value" ) );
+
+    $('#amount2').change(function(){
+      var val = $(this).val();
+      $('#slider-range-max2').slider('value',val);
+    });    
+    $( "#slider-range-max2" ).slider({
+      range: "max",
+      value: 3,
+      min: 0,
+      max: 10,
+      slide: function( event, ui ) {
+        $( "#amount2" ).val(ui.value );
+      }
+    });
+    $( "#amount2" ).val($( "#slider-range-max2" ).slider( "value" ) );
+
+  });
+
+ 
+
+//Selectmenu
+
+  $(function() {
+    $( "#selectmenu1" ).selectmenu();
+  });    
